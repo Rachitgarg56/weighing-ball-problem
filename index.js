c220827f7efaac9167bcacc768dff0291cfa1b48
Rachit Garg <rachitgarg56@gmail.com>
2024-08-02 14:28:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// parameters -> number of balls
+// returns -> number of times weighed on machine
+
+function minWeighings(n) {
+    let count = 0;
+    while (n > 1) {
+      count++;
+      n = Math.ceil(n / 3);
+    }
+    return count;
+}
+
+console.log(minWeighings(100));
+
+// dividing the balls in 3 groups is more optimal way because it gives us more information
+// Ex: If we have 3 groups of balls, the first 2 groups are weighed on weighing machine, then there will be two cases, whether they are equal in weight or unequal
+// if equal in weight, the light ball present in third group else in the less weighed group
+
